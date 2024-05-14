6fdda02a5d52f5dde392dc46f6843c5d576b9d85
朴睦 <927561237@qq.com>
2024-05-14 22:59:57 +0800

    maven 生命周期

--- _maven-project01/src/main/java/vip/dengwj/HelloWorld.java
+++ _maven-project01/src/main/java/vip/dengwj/HelloWorld.java
@@ -1,7 +1,18 @@
 package vip.dengwj;
 
+import org.slf4j.Logger;
+
 public class HelloWorld {
     public static void main(String[] args) {
+        Logger logger;
         System.out.println("Hello Maven");
+    }
+
+    public static void test1() {
+        System.out.println("01-test1");
+    }
+
+    public static void test2() {
+        System.out.println("01-test2");
     }
 }
---
+++ _maven-project01/src/test/java/pm/ww/DemoTest.java
@@ -0,0 +1,16 @@
+package pm.ww;
+
+import org.junit.Test;
+import vip.dengwj.HelloWorld;
+
+public class DemoTest {
+    @Test
+    public void testTest1() {
+        HelloWorld.test1();
+    }
+
+    @Test
+    public void testTest2() {
+        HelloWorld.test2();
+    }
+}
