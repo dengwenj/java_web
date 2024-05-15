b08188cbf125cc8957418259d6a33d8782795565
朴睦 <927561237@qq.com>
2024-05-15 15:56:14 +0800

    创建 SpringBootApplication

---
+++ springboot-web-quicjstart/src/main/java/vip/dengwj/SpringbootWebQuicjstartApplication.java
@@ -0,0 +1,12 @@
+package vip.dengwj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 启动类：启动 springboot 工程
+@SpringBootApplication
+public class SpringbootWebQuicjstartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebQuicjstartApplication.class, args);
+    }
+}
