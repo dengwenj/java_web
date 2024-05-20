37b05f5e60433945a484060918caa80e948d6512
朴睦 <927561237@qq.com>
2024-05-20 22:37:07 +0800

    mybatis crud 创建

---
+++ spring-boot-mybatis-crud/src/main/java/vip/dengwj/SpringBootMybatisCrudApplication.java
@@ -0,0 +1,13 @@
+package vip.dengwj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMybatisCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisCrudApplication.class, args);
+    }
+
+}
---
+++ spring-boot-mybatis-crud/src/main/java/vip/dengwj/mapper/EmpMapper.java
@@ -0,0 +1,8 @@
+package vip.dengwj.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class EmpMapper {
+
+}
