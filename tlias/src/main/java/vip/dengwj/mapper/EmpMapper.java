646673177641308a2627d08b7713ecfc5fbafb18
朴睦 <927561237@qq.com>
2024-05-22 22:41:17 +0800

    pagehelper 分页插件

--- tlias/src/main/java/vip/dengwj/mapper/EmpMapper.java
+++ tlias/src/main/java/vip/dengwj/mapper/EmpMapper.java
@@ -2,6 +2,7 @@ package vip.dengwj.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import vip.dengwj.pojo.Emp;
 
 import java.util.List;
@@ -17,9 +18,13 @@ public interface EmpMapper {
 //        Integer pageSize,
 //        Emp emp
 //    );
-    List<Emp> getEmpList(
-        @Param("page") Integer page,
-        @Param("pageSize") Integer pageSize,
-        @Param("emp") Emp emp
-    );
+//    List<Emp> getEmpList(
+//        @Param("page") Integer page,
+//        @Param("pageSize") Integer pageSize,
+//        @Param("emp") Emp emp
+//    );
+
+    // 使用 pagehelper 分页插件获取列表
+    @Select("select * from talias.emp")
+    List<Emp> list();
 }
--- tlias/src/main/java/vip/dengwj/service/impl/EmpServiceImpl.java
+++ tlias/src/main/java/vip/dengwj/service/impl/EmpServiceImpl.java
@@ -1,5 +1,6 @@
 package vip.dengwj.service.impl;
 
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import vip.dengwj.mapper.EmpMapper;
@@ -14,14 +15,26 @@ public class EmpServiceImpl implements EmpService {
     @Autowired
     EmpMapper empMapper;
 
+//    @Override
+//    public Page<Emp> getEmpList(Integer page, Integer pageSize, Emp emp) {
+//        Integer formatPage = (page - 1) * pageSize;
+//        // 获取员工列表
+//        List<Emp> list = empMapper.getEmpList(formatPage, pageSize, emp);
+//        // 获取员工数量
+//        long total = empMapper.getEmpCount();
+//
+//        return new Page<>(total, list);
+//    }
+
+    // 使用 pagehelper 分页插件获取列表
     @Override
     public Page<Emp> getEmpList(Integer page, Integer pageSize, Emp emp) {
-        Integer formatPage = (page - 1) * pageSize;
-        // 获取员工列表
-        List<Emp> list = empMapper.getEmpList(formatPage, pageSize, emp);
-        // 获取员工数量
-        long total = empMapper.getEmpCount();
+        PageHelper.startPage(page, pageSize);
 
-        return new Page<>(total, list);
+        List<Emp> list = empMapper.list();
+
+        com.github.pagehelper.Page<Emp> page1 = (com.github.pagehelper.Page<Emp>) list;
+
+        return new Page<>(page1.getTotal(), page1.getResult());
     }
 }
