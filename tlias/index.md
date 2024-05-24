8cc06dcd1f1ebdca7be610a4c411cacfe7386247
朴睦 <927561237@qq.com>
2024-05-24 17:03:50 +0800

    文件上传

--- tlias/index.md
+++ tlias/index.md
@@ -14,3 +14,18 @@ public class UploadController {
     }
 }
 ```
+
+## 本地存储
+* 在服务端，接收到上传上来的文件之后，将文件存储在本地服务器磁盘中
+* 在 Springboot 中，文件上传，默认单个文件允许最大大小为 1M，如果需要上传大文件，可以进行如下配置：
+* 配置单个文件最大上传大小：spring.servlet.multipart.max-file-size=10MB
+* 配置单个请求最大上传大小（一次请求可以上传多个文件）：spring.servlet.multipart.max-request-size=100MB
+* MultipartFile 对象的方法：
+* 获取原始文件名：String getOrigininalFilename()
+* 将接收的文件转存到磁盘文件中：void transferTo(File dest)
+* 获取文件的大小，单位：字节：long getSize()
+* 获取文件内容的字节数组：byte[] getBytes()
+* 获取接收到的文件内容的输入流：InputStream getInputStream()
+
+## 本地存储缺点
+* 无法直接访问，磁盘满了，磁盘坏了
--- tlias/src/main/java/vip/dengwj/controller/UploadController.java
+++ tlias/src/main/java/vip/dengwj/controller/UploadController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import vip.dengwj.pojo.Result;
 
+import java.io.File;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class UploadController {
@@ -13,9 +16,14 @@ public class UploadController {
      * 文件上传
      */
     @PostMapping("/upload")
-    public Result upload(String name, String password, MultipartFile file) {
-      log.info("文件上传：{}, {}, {}", name, password, file);
-
-      return Result.success();
+    public Result upload(MultipartFile file) throws Exception {
+        log.info("文件上传：{}",file);
+        // 获取原始文件名
+        String originalFilename = file.getOriginalFilename();
+        // 新的文件名
+        String newName = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
+        // 将文件存储到磁盘
+        file.transferTo(new File("/Users/dengwenjie/Desktop/" + newName));
+        return Result.success();
     }
 }
