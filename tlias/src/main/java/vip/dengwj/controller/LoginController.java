e529e3111306d1156cb4674ccf0af312bb42c101
朴睦 <927561237@qq.com>
2024-05-25 18:47:29 +0800

    登录成功返回 token

--- tlias/src/main/java/vip/dengwj/controller/LoginController.java
+++ tlias/src/main/java/vip/dengwj/controller/LoginController.java
@@ -8,16 +8,35 @@ import org.springframework.web.bind.annotation.RestController;
 import vip.dengwj.pojo.Emp;
 import vip.dengwj.pojo.Result;
 import vip.dengwj.service.EmpService;
+import vip.dengwj.utils.JWTUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
 public class LoginController {
     @Autowired
     EmpService empService;
+    @Autowired
+    JWTUtils jwtUtils;
 
     @PostMapping("/login")
     public Result login(@RequestBody Emp emp) {
         Emp emp1 = empService.login(emp);
-        return emp1 != null ? Result.success() : Result.error("用户名或密码错误");
+
+        // 登录成功返回令牌
+        if (emp1 != null) {
+            Map<String, Object> data = new HashMap<>();
+            data.put("id", emp1.getId());
+            data.put("name", emp1.getName());
+            data.put("username", emp1.getUsername());
+
+            String jwt = jwtUtils.generateToken(data);
+            return Result.success(jwt);
+        }
+
+        // 登录失败
+        return Result.error("用户名或密码错误");
     }
 }
---
+++ tlias/src/main/java/vip/dengwj/utils/JWTUtils.java
@@ -0,0 +1,46 @@
+package vip.dengwj.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "myjwt")
+public class JWTUtils {
+    private String token;
+    private Long expiration;
+
+    /**
+     * 生成 jwt
+     *
+     * @param claims
+     * @return
+     */
+    public String generateToken(Map<String, Object> claims) {
+        String jwt = Jwts.builder()
+            .signWith(SignatureAlgorithm.HS256, token)
+            .setClaims(claims)
+            .setExpiration(new Date(System.currentTimeMillis() * expiration))
+            .compact();
+
+        return jwt;
+    }
+
+    /**
+     * 解析 jwt
+     */
+    public Map<String, Object> parseToken(String jwt) {
+        Claims body = Jwts.parser()
+            .setSigningKey(token)
+            .parseClaimsJws(jwt)
+            .getBody();
+        return body;
+    }
+}
