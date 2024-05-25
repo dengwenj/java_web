b168d1d52ab259d665e06169b8acaf511988a6a7
朴睦 <927561237@qq.com>
2024-05-25 22:20:49 +0800

    Filter 过滤器链（就是可以有多个中间件）

---
+++ tlias/src/main/java/vip/dengwj/filter/WWFilter.java
@@ -0,0 +1,16 @@
+package vip.dengwj.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class WWFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("过滤器链前");
+        filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("过滤器链后");
+    }
+}
